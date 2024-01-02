37a14314dc0ba8f929730298c63dcdd5d21e2f33
EricYanNaing <ericthedev97@gmail.com>
2024-01-02 18:55:15 +0630

    map array object

--- src/components/Frutis.jsx
+++ src/components/Frutis.jsx
@@ -1,10 +1,28 @@
-const fruits = ['mango','banana','orange']
-export default function Fruits(){
-    return <>
-        <ul>
-            {fruits.map((fruit) => 
-            <li>{fruit}</li>
-            )}
-        </ul>
+// const fruits = ["mango", "banana", "orange"];
+const fruits = [
+  {
+    name: "mango",
+    price: 5,
+  },
+  {
+    name: "banana",
+    price: 3,
+  },
+  {
+    name: "orange",
+    price: 4,
+  },
+];
+export default function Fruits() {
+  return (
+    <>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.name}>
+            {fruit.name} = {fruit.price} $
+          </li>
+        ))}
+      </ul>
     </>
+  );
 }
