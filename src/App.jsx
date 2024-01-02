9625c30523d7f0ab68f1b2d31a1d70035e39be08
EricYanNaing <ericthedev97@gmail.com>
2024-01-02 19:14:52 +0630

    conditional render using element variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Hello from "./components/Hello";
 import Fruits from "./components/Frutis";
 import ConditionalComponent from "./components/ConditionalComponent";
+import Conditional from "./components/Conditional";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +16,8 @@ function App() {
     <div className="App">
       {/* <Hello person={person} /> */}
       {/* <Fruits /> */}
-      <ConditionalComponent />
+      {/* <ConditionalComponent /> */}
+      <Conditional />
     </div>
   );
 }
